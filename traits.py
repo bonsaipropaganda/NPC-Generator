f772c4bd0905f105f2771e32b9fe95fd06e730e0
bonsaipropaganda <116498290+bonsaipropaganda@users.noreply.github.com>
2023-04-11 22:52:01 +0530

    Added files via upload

---
+++ traits.py
@@ -0,0 +1,295 @@
+trait_string = """
+kindness
+leadership
+consideration
+empathy
+reliable
+intelligent
+innovative
+thoughtful
+conscientious
+effective
+resilient
+brave
+calm
+optimistic
+humorous
+pretty eyes
+fit body
+nice smile
+cute dimples
+smooth legs
+"good genes"
+fast metabolism
+flexible
+fast runner
+strong
+good cook
+good painter
+fast reader
+good with computers
+fast typer
+good doctor
+good teacher
+good pilot
+good mechanic
+good accountant
+Warm
+Friendly
+Clean
+Honest
+Loyal
+Trustworthy
+Dependable
+Open-Minded
+Thoughtful
+Wise
+Mature
+Ethical
+Courageous
+Constructive
+Productive
+Progressive
+Individualistic
+Observant
+Neat
+Punctual
+Logical
+Prompt
+Accurate
+Self-Reliant
+Independent
+Inventive
+Wholesome
+Attentive
+Frank
+Purposeful
+Realistic
+Adventurous
+Relaxed
+Curious
+Modern
+Charming
+Modest
+Enthusiastic
+Polite
+Patient
+Talented
+Perceptive
+Forgiving
+Ambitious
+Respectful
+Grateful
+Resourceful
+Courteous
+Helpful
+Appreciative
+Imaginative
+Self-Disciplined
+Decisive
+Humble
+Self-Confident
+Easygoing
+Consistent
+Positive
+Artistic
+Fashionable
+Convincing
+Thrifty
+Bold
+Suave
+Methodical
+Interesting
+Unselfish
+Responsible
+Reasonable
+Likable
+Clever
+Cooperative
+Romantic
+Proficient
+Apathetic
+Argumentative
+Cowardly
+Impulsive
+Self-centered
+Dishonest
+Unable to empathize
+Jealous
+Sarcastic
+Arrogant
+Inattentive
+Lazy
+Rigid
+Irresponsible
+Forgetful
+Impatient
+Indecisive
+Inflexible
+Weak-willed
+Foolish
+Unattractive
+Disagreeable
+Plain-looking
+Unpleasant
+Ill-favored
+Unfit
+Unhealthy
+Hideous
+Out of shape
+Weak
+Unstyled hair
+Ugly face
+Bad teeth
+Too short
+Unathletic
+Slight
+Bad posture
+Bad skin
+Overweight
+Poor figure
+Rude
+Egocentric
+Greedy
+Biased
+Aggressive
+Disrespectful
+Manipulative
+Shy
+Untimely
+Careless
+Irresponsible
+Narrow-minded
+Unfocused
+Bossy
+Disloyal
+Nervous
+Unpredictable
+Narcissistic
+Too certain
+Selfishness
+Perfectionism
+Overly independence
+Indecisive Impatience
+Too ambitious
+Ambitious
+Sensitive
+Procrastinator
+Neurotic
+Pessimistic
+Messy
+Cynical
+Arrogance
+Cruelty
+Bad temperament
+Disagreeableness
+Bossy attitude
+Selfishness
+Observant
+Self-reserved
+Stylish
+Strange
+Serious
+Foolish
+Pessimistic
+Foolhardy
+Perverse
+Bitchy
+Indecisive
+Selfish
+Careless
+Lazy
+Superficial
+Clinging
+Materialistic
+Thoughtless
+Grumpy
+Possessive
+Boastful
+Indiscreet
+Silly
+Confrontational
+Miserly
+Touchy
+Cynical
+Nervous
+Untrustworthy
+Bossy
+Intolerant
+Stubborn
+Finicky
+Parsimonious
+Cowardly
+Narrow-minded
+Unkind
+Aggressive
+Impatient
+Rude
+Dishonest
+Obstinate
+Vulgar
+Cantankerous
+Jealous
+Sullen
+Deceitful
+Obnoxious
+Vague
+Changeable
+Machiavellian
+Tactless
+Conservative
+Moody
+Truculent
+Fussy
+Picky
+Dogmatic
+Overcritical
+Weak-Willed
+Big-Headed
+Inconsistent
+Self-Centred
+Aloof
+Impolite
+Ruthless
+Harsh
+Quarrelsome
+Gullible
+Pusillanimous
+Boring
+Interfering
+Stingy
+Domineering
+Overemotional
+Crue
+Nasty
+Unreliable
+Callous
+Irresponsible
+Stupid
+Belligerent
+Inconsiderate
+Secretive
+Cunning
+Naughty
+Untidy
+Arrogant
+Impulsive
+Sarcastic
+Bone-İdle
+Inflexible
+Sneaky
+Flirtatious
+Patronizing
+Compulsive
+Mean
+Timid
+Greedy
+Pompous
+Idle
+Resentful
+Detached
+Obsessive
+Vain
+"""
+
+trait_list = trait_string.split("\n")
